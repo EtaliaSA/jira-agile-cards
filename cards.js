79f194c2846e6c44d5e1d1653f1718062da3282e
Daniele Madama <d.madama@gmail.com>
2015-03-20 15:13:11 +0100

    Get estimation value

--- cards.js
+++ cards.js
@@ -23,7 +23,7 @@ function print(xml) {
   var priorityIcon = domAttribute(item, 'priority', 'iconUrl');;
   var title = domValue(item, 'summary');
   var description = domValue(item, 'description');
-  var sp_estimate = "";
+  var sp_estimate = getItemCustomField(item, 'Story Points');
   var sp_remaining = "";
   // Clone template and populate it
   var template = document.getElementById("card-template").cloneNode(true);
@@ -32,6 +32,7 @@ function print(xml) {
   template.getElementsByTagName("img")[1].setAttribute("src", priorityIcon);
   template.getElementsByTagName("header")[0].getElementsByTagName("span")[0].innerHTML = id;
   template.getElementsByTagName("header")[0].getElementsByTagName("span")[1].innerHTML = title;
+  template.getElementsByTagName("footer")[0].getElementsByTagName("span")[1].innerHTML = sp_estimate;
   template.getElementsByTagName("article")[0].innerHTML = description;
   document.getElementById("cards").appendChild(template);
 }
@@ -59,4 +60,19 @@ function domValue(el, name) {
 
 function domAttribute(el, name, attr) {
   return el.getElementsByTagName(name)[0].getAttribute(attr);
+}
+
+function getItemCustomField(item, name) {
+  var fields = item.getElementsByTagName("customfields")[0].childNodes;
+  for (var i=0; i<fields.length; i++) {
+    var field = fields[i];
+    if (field.nodeType == 3) {
+      continue;
+    }
+    var fieldName = domValue(field, "customfieldname");
+    if (fieldName == name) {
+      return domValue(field.getElementsByTagName("customfieldvalues")[0], "customfieldvalue");
+    }
+  }
+  return null;
 }
